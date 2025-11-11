738c809f34db42101bb0a83e6e7e322a3d209af8
Jay Lee <jaylee0717@tamu.edu>
2025-11-10 20:17:07 -0600

    Add Backbone comments from HW2

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -20,8 +20,25 @@ arma::uvec MyKmeans_c(const arma::mat& X, int K,
     
     // Initialize any additional parameters if needed
     
-    // For loop with kmeans algorithm
     
+    // Implement K-means algorithm. 
+    // It should stop when either 
+    // (i) the centroids don't change from one iteration to the next (exactly the same), or
+    // (ii) the maximal number of iterations was reached, or
+    // (iii) one of the clusters has disappeared after one of the iterations (in which case the error message is returned)
+    
+    // Precompute X squared since doesnt need to be repeated
+    
+    // For loop with kmeans algorithm
+    for (int iter = 0; iter < numIter; iter++) {
+      // Compute distances from each row in X and M, store it in a matrix nxK matrix D2
+      
+      // Assign value to Y by finding at which column the minimum is at in D
+      // then check number of unique values in Y is K
+      
+      // Compute new centroid values mu, check that it changed, and store into M
+
+    }
     
     // Returns the vector of cluster assignments
     return(Y);
